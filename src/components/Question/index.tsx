6874ae1eb0efea8ad352bd63056ed18c0ee6fc63
Anderson Santos de Farias <andersonsantofarias@outlook.com>
2021-11-26 14:45:26 -0300

    feat: Add option to check if the question is answered and highlighted.

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import cx from 'classnames'
 import './styles.scss'
 
 type QuestionProps = {
@@ -8,11 +9,25 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isHighlighted: boolean;
+  isAnswered: boolean;
 }
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({
+  content,
+  author,
+  children,
+  isHighlighted = false,
+  isAnswered = false,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import { useParams, useHistory } from 'react-router'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 import { Button } from '../components/Button'
 import { Question } from '../components/Question/index'
 import { RoomCode } from '../components/RoomCode'
-import { useAuth } from '../hooks/useAuth'
 import { useRoom } from '../hooks/useRoom'
 
 import '../styles/room.scss'
@@ -31,11 +31,24 @@ export function AdminRoom() {
     history.push('/')
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true
+    })
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true
+    })
+  }
+
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm("Você tem certeza que deseja excluir esta pergunta?")) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
     }
   }
+
 
   return (
     <div id="page-room">
@@ -62,7 +75,25 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
